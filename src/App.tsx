69d6845dce46c5b21e1779bed3f1c760b0acd7d0
yunusnedim <yunus.mehel@volvocars.com>
2026-01-28 14:19:53 +0100

    Renders ListGroup content from online

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import ListGroup from "./components/ListGroup";
+import ListGroup from './components/ListGroup';
 
 function App() {
-    return <div>  <ListGroup /></div>;
+    return (
+        <ul className='list-group'>
+            <li className='list-group-item active'>Cras justo odio</li>
+            <li className='list-group-item'>Dapibus ac facilisis in</li>
+            <li className='list-group-item'>Morbi leo risus</li>
+            <li className='list-group-item'>Porta ac consectetur ac</li>
+            <li className='list-group-item'>Vestibulum at eros</li>
+        </ul>
+    );
 }
 
 export default App;
