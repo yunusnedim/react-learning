c8ed5f054e35c4ba37dbdf517d4765a81d6039c5
yunusnedim <yunus.mehel@volvocars.com>
2026-01-28 13:48:44 +0100

    Fixes App not rendering anything

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Message from './Message';
 
 function App() {
     // return <div>  </Message /></div>;
-    return;
-    <div>
-        <Message />
-    </div>;
+    return (
+        <div>
+            {' '}
+            <Message />{' '}
+        </div>
+    );
 }
 
 export default App;
