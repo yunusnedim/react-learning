1a632b9fc5e4e0c069491ab985173b86b9785d04
yunusnedim <yunus.mehel@volvocars.com>
2026-01-29 11:19:50 +0100

    Moves code to ListGroup (was in App)

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,7 @@ import ListGroup from './components/ListGroup';
 import {Fragment, useState} from 'react';
 
 function App() {
-    const items = ['New York', 'SF', 'Tokyo', 'Paris'];
-
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-    //Hook
-
-    return (
-        //<></> makes this a fragment
-        <>
-            <h1>List</h1>
-            {/* {items.length === 0 ? <p>No Item Found</p> : null} same as below */}
-            {items.length === 0 && <p>No Item Found</p>}
-            <ul className='list-group'>
-                {items.map((item, index) => (
-                    <li
-                        className={
-                            selectedIndex === index
-                                ? 'list-group-item active'
-                                : 'list-group-item'
-                        }
-                        key={item}
-                        onClick={() => {
-                            setSelectedIndex(index);
-                        }}
-                    >
-                        {item}
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+    return <ListGroup />;
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,36 @@
+import {useState} from 'react';
+
 function ListGroup() {
-  return <h1>List Group</h1>
+    const items = ['New York', 'SF', 'Tokyo', 'Paris'];
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    //Hook
+
+    return (
+        //<></> makes this a fragment
+        <>
+            <h1>List</h1>
+            {/* {items.length === 0 ? <p>No Item Found</p> : null} same as below */}
+            {items.length === 0 && <p>No Item Found</p>}
+            <ul className='list-group'>
+                {items.map((item, index) => (
+                    <li
+                        className={
+                            selectedIndex === index
+                                ? 'list-group-item active'
+                                : 'list-group-item'
+                        }
+                        key={item}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                        }}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default ListGroup;
