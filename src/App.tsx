4f7b4fd1c94066ccdd837aae0e31c73d7f1afe73
yunusnedim <yunus.mehel@volvocars.com>
2026-01-29 10:13:00 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import ListGroup from './components/ListGroup';
 import {Fragment} from 'react';
 
 function App() {
-    let items = ['New York', 'SF', 'Tokyo', 'Paris'];
-    items = [];
+    const items = ['New York', 'SF', 'Tokyo', 'Paris'];
 
-    const getMessage = () => {
-        return items.length === 0 ? <p>No Item Found</p> : null;
-    };
     return (
         //<></> makes this a fragment
         <>
             <h1>List</h1>
-            {getMessage()}
+            {/* {items.length === 0 ? <p>No Item Found</p> : null} same as below */}
+            {items.length === 0 && <p>No Item Found</p>}
             <ul className='list-group'>
                 {items.map((item) => (
                     <li key={item}>{item}</li>
